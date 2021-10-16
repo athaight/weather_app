8b2b7657b403224b51fdcb4b1aa3ede9854cb8f8
Andrew Haight <haight.haight@gmail.com>
2021-10-16 15:08:56 -0700

    style adjuetments

--- logic.js
+++ logic.js
@@ -44,3 +44,11 @@ search: function () {
 document.querySelector('.search button').addEventListener('click', function () {
 weather.search();
 })
+
+document.querySelector('.search-bar').addEventListener('keyup', function(e) {
+    if (e.key == "Enter") {
+        weather.search();
+}
+});
+
+weather.FetchWeather('Denver');
