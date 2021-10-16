1e66c093080a29ce94b8f15a57b4189f28dd7345
Andrew Haight <haight.haight@gmail.com>
2021-10-16 13:30:58 -0700

    logic.js gets weather / restyle no materalize

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 const apiKey = "8b4d320427abe778710968ffa0a03a6c"
-const oneCall = "https://api.openweathermap.org/data/2.5/onecall?&units=imperial&";
-const geoCords = "http://api.openweathermap.org/geo/1.0/direct?&units=imperial&";
+const oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${ apiKey }`;
+const geoCords = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${ apiKey }`;
 const today = new Date();
 let day = String(today.getDate()).padStart(2, '0');
 let month = String(today.getMonth() + 1).padStart(2, '0'); 
@@ -15,14 +15,21 @@ let temp = document.querySelector('temp');
 let wind = document.querySelector('wind');
 let humidity = document.querySelector('humidity');
 let uvi = document.querySelector('uvi');
-
-function getCity(city){
-const params = new URLSearchParams({ q: city, appid: apiKey });
-fetch(geoCords + params)
-.then((response) => response.json())
-.then(function  (data) {
-    console.log(data);
-    
-
-})
+let weather = {
+FetchWeather: function(city) {
+    fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city + "&units=imperial&appid=" 
+        + apiKey
+    )
+    .then((response) => response.json())
+    .then((data) => this.displayWeather(data))
+},
+displayWeather: function(data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name,icon,description,temp,humidity,speed)
+}
 }
